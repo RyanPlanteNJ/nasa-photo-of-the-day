cf24d9bd8cddaad4dc4e390e3bfeb2895193ded6
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-04-14 20:49:03 -0400

    changing file tree

---
+++ src/components/NasaContainer/NasaContainer.jsx
@@ -0,0 +1,27 @@
+import react, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NasaExplain from './NasaExplain'
+import NasaTitle from './NasaTitle'
+import NasaImage from '../NasaImage/NasaImage'
+
+
+export default function NasaContainer() {
+    const [nasaData, setNasaData] = useState('');
+    const [date, setDate] = useState('');
+
+    useEffect(() => {
+        .get('https://api.nasa.gov/planetary/apod?api_key=FhTSqyczwbm7Mscx4U6PJLD00cxsZ9ItIPow4p2v')
+        .then(response => {
+            setNasaData(response.data);
+        });
+        
+    },[]);
+
+    return (
+        <div className = "nasaContainer">
+            <NasaTitle title={nasaData.title} />
+            <NasaImage image ={nasaData} />
+            <NasaExplain explain={nasaData.explain} />
+        </div>
+    )
+}
