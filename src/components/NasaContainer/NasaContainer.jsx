224c4e3e5e1be69b3ca26922b56f7e5a1e46fbef
RyanPlanteNJ <ryanm.plante@gmail.com>
2020-04-16 22:53:00 -0400

    adding date func

--- src/components/NasaContainer/NasaContainer.jsx
+++ src/components/NasaContainer/NasaContainer.jsx
@@ -3,26 +3,34 @@ import axios from 'axios';
 import NasaExplain from './NasaExplain';
 import NasaTitle from './NasaTitle';
 import NasaImage from '../NasaImage/NasaImage';
+import Date from "./Date"
 
 
 export default function NasaContainer() {
     const [nasaData, setNasaData] = useState('');
+    const [date, setDate] = useState('');
 
+    const dateSubmit = e => {
+        e.preventDefault();
+        setDate(document.querySelector("#date").value);
+    };
+    
     useEffect(() => {
         axios
-            .get('https://api.nasa.gov/planetary/apod?api_key=FhTSqyczwbm7Mscx4U6PJLD00cxsZ9ItIPow4p2v')
+            .get(`https://api.nasa.gov/planetary/apod?api_key=FhTSqyczwbm7Mscx4U6PJLD00cxsZ9ItIPow4p2v&date=${date}`) //use &${props.data}
             .then(response => {
                 // console.log(response.data);
                 setNasaData(response.data);
             });
         
-    },[]);
+    },[date]);
 
     return (
        <div className = "nasaContainer">
-            <NasaTitle title={nasaData.title} />
-            <NasaImage image ={nasaData} />
-            <NasaExplain explain={nasaData.explanation} />
+        <Date dateSubmit={dateSubmit}/>
+        <NasaTitle title={nasaData.title} />
+        <NasaImage image ={nasaData} />
+        <NasaExplain explain={nasaData.explanation} />
         </div>
     )
 }
--- src/components/NasaHeader/NasaHeader.jsx
+++ src/components/NasaHeader/NasaHeader.jsx
@@ -4,19 +4,16 @@ import styled from 'styled-components'
 
 const LogoContainer = styled.div`
     display: flex;
-    justify-content: center;
+    flex-flow: row;
+    justify-content: space-between;
     align-items: center;
 `;
 
-const LogoImage = styled.img`
-    justify-content: 
-
-`
 
 export default function NasaHeader() {
     return (
         <LogoContainer>
-            <img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt ="Nasa Logo"/>
+            <a href="https://nasa.gov"><img src="https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg" alt ="Nasa Logo"/></a>
             <h1>Photo of the Day </h1>
         </LogoContainer>
     )
